206bb8ae537c192664e234f0252be014eb9b80dd
yokrh <y.oka.gml@gmail.com>
2019-03-10 02:28:47 +0900

    to PWA

---
+++ src/serviceworker.js
@@ -0,0 +1,11 @@
+// application installation
+self.addEventListener('install', event => {
+});
+
+// application activated
+self.addEventListener('activate', event => {
+});
+
+// application fetch network data
+self.addEventListener('fetch', event => {
+});
